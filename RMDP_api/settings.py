53ca72d1b9a089c444f26af719ba932292ff124f
DanielDu <danghoangnhan.1@gmail.com>
2021-07-23 18:17:54 +0800

    add  mongo django framework

--- RMDP_api/settings.py
+++ RMDP_api/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'RMDP_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'RMDP',
+        'CLIENT': {
+            'host': 'mongodb://127.0.0.1:27017',
+            'username': 'admin',
+            'password': 'admin',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
