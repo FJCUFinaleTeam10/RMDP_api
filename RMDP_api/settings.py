927e95ee87ae5bcd7ca393ee7d9d25c1bfcfc230
DanielDu <danghoangnhan.1@gmail.com>
2021-07-24 18:27:38 +0800

    fix the duplicated cors

--- RMDP_api/settings.py
+++ RMDP_api/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
     'rest_framework_mongoengine',
     'order',
