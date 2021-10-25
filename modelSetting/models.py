ffdf381c05f25d24df97445c71aaaf8a810311ff
DanielDu <danghoangnhan.1@gmail.com>
2021-10-25 22:40:36 +0800

    update model

--- modelSetting/models.py
+++ modelSetting/models.py
@@ -3,6 +3,13 @@ from mongoengine import *
 
 # Create your models here.
 class RMDP_env(Document):
-    
+    Order_num = IntField()
+    all_driver_routing_Theta_x = ListField()
+    capacity = IntField()
+    deadlineTime = IntField()
+    delay = IntField()
+    maxLengthPost = IntField()
+    maxtime_of_system_
+
 
 
