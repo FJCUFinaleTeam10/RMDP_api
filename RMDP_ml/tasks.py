fea315ccb55e5a4f07319bf9c38cdac38dd65977
DanielDu <danghoangnhan.1@gmail.com>
2021-10-28 02:40:07 +0800

    fix the task unregister

--- RMDP_ml/tasks.py
+++ RMDP_ml/tasks.py
@@ -1,23 +1,20 @@
 from celery import shared_task
 
-
-
 from RMDP_ml import new_core
 from RMDP_ml import userSimulator
 from RMDP_ml import driverSimulator
 
-'''
+
 @shared_task
 def run_RMDP():
     new_core.generateThread()
-'''
+
 
 @shared_task
 def generatingOrder():
     userSimulator.generateThread()
 
-'''
+
 @shared_task
 def updateDriver():
     driverSimulator.generateThread()
-'''
