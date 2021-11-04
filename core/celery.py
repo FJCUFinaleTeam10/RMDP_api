bd917ef08b50ad7befc69a9222687950d90dd0f5
DanielDu <danghoangnhan.1@gmail.com>
2021-11-04 10:01:49 +0800

    fix orderslave docker

--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("core", broker=[os.environ.get("CELERY_BROKER")])
 # Using a string here means the worker doesn't have to serialize
 app.config_from_object("django.conf:settings", namespace="CELERY")
