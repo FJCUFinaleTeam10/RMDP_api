64e0c527e824bea141b9f55985bd53dd6028999e
DanielDu <danghoangnhan.1@gmail.com>
2021-12-16 06:48:23 +0000

    add history views

--- history/serializers.py
+++ history/serializers.py
@@ -1,8 +1,20 @@
+from rest_framework.utils.serializer_helpers import ReturnDict
 from rest_framework_mongoengine.serializers import DocumentSerializer
 from geolocation.models import *
 from history.models import history
 
 
 class HistorySerializer(DocumentSerializer):
+
+    # @property
+    # def data(self):
+    #     ret = super(HistorySerializer, self).data
+    #     return ReturnDict(ret, serializer=self)
+    #
+    # # In class ReturnDict(OrderedDict)
+    # def __init__(self, *args, **kwargs):
+    #     self.serializer = kwargs.pop('serializer')
+    #     super(ReturnDict, self).__init__(*args, **kwargs)
+
     class Meta:
         model = history
--- history/views.py
+++ history/views.py
@@ -1,4 +1,4 @@
-from dateutil.parser import parser
+from dateutil import parser
 from django.db import IntegrityError
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
@@ -7,17 +7,32 @@ from driver.models import driver
 from history.models import history
 from history.serializers import HistorySerializer
 from geolocation.serializers import *
+from rest_framework_mongoengine import serializers
 
 
 @api_view(['POST'])
 def getHistoryBaseOnCityList(request):
-    cityID = int(request.data['params'].get('cityID', None))
-    fromDate = parser.parse(request.data['params'].get('from', None))
-    toDate = parser.parse(request.data['params'].get('to', None))
-    driverList = list(driver.objects(City_id=cityID).values_list('Driver_ID'))
-    routehistoryList = history.objects(D_id__in=driverList)
-    result = HistorySerializer(routehistoryList, many=True)
-    response = {}
-    response['data'] = result.data
-    response["Access-Control-Allow-Origin"] = "*"
-    return JsonResponse(response)
+    try:
+        cityID = int(request.data['params'].get('cityID', None))
+        method = int(request.data['params'].get('method', None))
+        method+=1
+        fromDate = parser.parse(request.data['params'].get('from', None))
+        toDate = parser.parse(request.data['params'].get('to', None))
+        driverList = list(driver.objects(City_id=cityID).values_list('Driver_ID'))
+        response = {}
+        historyList = history.objects(
+            Q(D_id__in=driverList) & Q(Time__gte=fromDate) & Q(Time__lte=toDate) & Q(Method=method))
+        result = HistorySerializer(historyList, many=True)
+        # json_data = [ob.to_mongo() for ob in historyList]
+        # response['data'] = serializers.serialize("json",historyList, many=True)
+        # response['data'] = dict(map(lambda driverID:(str(driverID),HistorySerializer(history.objects(D_id=driverID), many=True).data),driverList))
+        response["Access-Control-Allow-Origin"] = "*"
+        response['data'] = result.data
+        response['driverList'] = driverList
+        return JsonResponse(response)
+    except IntegrityError:
+        print(IntegrityError)
+    except MultipleObjectsReturned:
+        print(MultipleObjectsReturned)
+    except Exception as e:
+        raise print(e)
